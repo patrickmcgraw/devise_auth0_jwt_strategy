7f8abfa691d3ab0726b2d1d31f07e0052c70dae6
Patrick McGraw <patrick@mcgraw-tech.com>
2015-03-17 14:09:52 -0700

    move warden strategy init into a railtie

--- lib/devise_auth0_jwt_strategy.rb
+++ lib/devise_auth0_jwt_strategy.rb
@@ -1,15 +1,3 @@
 require 'devise_auth0_jwt_strategy/strategy'
 
-if ENV['AUTH0_CLIENT_SECRET']
-  puts "Wiring up Auth0 JWT Devise Strategy"
-
-  ::Devise.setup do |config|
-
-    config.warden do |manager|
-      manager.strategies.add(:auth0jwt, Devise::Strategies::Auth0Jwt)
-      manager.default_strategies(:scope => :user).unshift :auth0jwt
-    end
-
-  end
-
-end
+require 'devise_auth0_jwt_strategy/railtie' if defined?(::Rails)
---
+++ lib/devise_auth0_jwt_strategy/railtie.rb
@@ -0,0 +1,23 @@
+class DeviseAuth0JwtStrategy::Railtie < Rails::Railtie
+  initializer "devise_auth0_jwt_strategy.configure_rails_initialization" do
+    print "Wiring up Auth0 JWT Devise Strategy..."
+    if ENV['AUTH0_CLIENT_SECRET']
+
+      ::Devise.setup do |config|
+
+        config.warden do |manager|
+          manager.strategies.add(:auth0jwt, Devise::Strategies::Auth0Jwt)
+          manager.default_strategies(:scope => :user).unshift :auth0jwt
+        end
+
+      end
+
+      print "done.\n"
+
+    else
+      print " no Auth0 Secret Found. Skipping..."
+
+    end
+
+  end
+end
