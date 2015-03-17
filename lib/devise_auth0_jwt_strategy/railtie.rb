f2c3d5c9eac2ee4097150ba016230c006595714b
Patrick McGraw <patrick@mcgraw-tech.com>
2015-03-17 14:18:56 -0700

    Specifying encapsulating module for railtie

--- lib/devise_auth0_jwt_strategy/railtie.rb
+++ lib/devise_auth0_jwt_strategy/railtie.rb
@@ -1,23 +1,25 @@
-class DeviseAuth0JwtStrategy::Railtie < Rails::Railtie
-  initializer "devise_auth0_jwt_strategy.configure_rails_initialization" do
-    print "Wiring up Auth0 JWT Devise Strategy..."
-    if ENV['AUTH0_CLIENT_SECRET']
+module DeviseAuth0JwtStrategy
+  class Railtie < Rails::Railtie
+    initializer "devise_auth0_jwt_strategy.configure_rails_initialization" do
+      print "Wiring up Auth0 JWT Devise Strategy..."
+      if ENV['AUTH0_CLIENT_SECRET']
 
-      ::Devise.setup do |config|
+        ::Devise.setup do |config|
 
-        config.warden do |manager|
-          manager.strategies.add(:auth0jwt, Devise::Strategies::Auth0Jwt)
-          manager.default_strategies(:scope => :user).unshift :auth0jwt
+          config.warden do |manager|
+            manager.strategies.add(:auth0jwt, Devise::Strategies::Auth0Jwt)
+            manager.default_strategies(:scope => :user).unshift :auth0jwt
+          end
+
         end
+
+        print "done.\n"
+
+      else
+        print " no Auth0 Secret Found. Skipping..."
 
       end
 
-      print "done.\n"
-
-    else
-      print " no Auth0 Secret Found. Skipping..."
-
     end
-
   end
 end
